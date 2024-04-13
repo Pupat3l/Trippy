dc9866be7fa836a99ce6eff9d5725b80c3442cbc
Pujan Patel <94638467+Pupat3l@users.noreply.github.com>
2024-04-13 17:21:58 -0400

    project start

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage';
+function App() {
+  return (
+    <LandingPage />
+  );
+};
+
+export default App;
---
+++ my-app/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LandingPage() {
+    return (
+        <div>
+            <nav>
+                <h1>Trippy</h1>
+            </nav>
+        </div>
+    );
+};
