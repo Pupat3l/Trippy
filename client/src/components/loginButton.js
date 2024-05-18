65b14427919f9fcd0e88f760acfe5f413ab58907
Pujan Patel <94638467+Pupat3l@users.noreply.github.com>
2024-05-18 01:36:12 -0400

    changed redirect to navigate in loginbutton

--- client/src/components/loginButton.js
+++ client/src/components/loginButton.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react"; 
 import '../pages/Dashboard.css';
-import { redirect } from 'react-router-dom'; 
+import { Navigate } from 'react-router-dom'; 
 
 export default function LoginButton(){
     const {loginWithRedirect, isAuthenticated} = useAuth0();
 
     if (isAuthenticated) {
-        return <redirect to="/dashboard" />;
+        return <Navigate to="/dashboard" />;
       }
     return(
         !isAuthenticated && (
